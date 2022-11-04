6e66baaaebc8ae6acc428ec7161c8c36bf4aa7d9
DavidGentili <DavidGenntili@gmail.com>
2022-11-04 16:05:55 -0300

    Se agrego persistencia a atributos static del sistema

--- src/main/java/modelo/configEmpresa/PersistenciaConfiguracion.java
+++ src/main/java/modelo/configEmpresa/PersistenciaConfiguracion.java
@@ -14,6 +14,11 @@ public class PersistenciaConfiguracion implements Serializable {
     private ArrayList<Producto> productos;
     private ArrayList<OperarioDTO> operarios;
     private Sueldo sueldo;
+    //Variables encargadas de almacenar los valores estaticos de las clases
+    private int nroMozos;
+    private int nroOperarios;
+    private int nroProductos;
+
 
     public PersistenciaConfiguracion(String nombreLocal, ArrayList<Mozo> mozos, ArrayList<Mesa> mesas, ArrayList<Producto> productos, ArrayList<Operario> operarios, Sueldo sueldo){}
 
@@ -44,4 +49,22 @@ public class PersistenciaConfiguracion implements Serializable {
     public ArrayList<Operario> getOperarios() {
         return null;
     }
+
+    /**
+     * Retorna el numero de mozos (numero usado para obtenr id) almacenado
+     * @return Numero de mozos
+     */
+    public int getNroMozos() {return nroMozos;}
+
+    /**
+     * Retorna el numero de productos (numero usado para obtenr id) almacenado
+     * @return Numero de productos
+     */
+    public int getNroProductos(){ return nroProductos;}
+
+    /**
+     * Retorna el numero de operarios (numero usado para obtenr id) almacenado
+     * @return Numero de operarios
+     */
+    public int getNroOperarios(){return nroOperarios;}
 }
