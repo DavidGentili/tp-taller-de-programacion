acc538e0e9306d3c6c40b88272d65d90fd6872b1
DavidGentili <DavidGenntili@gmail.com>
2022-11-12 18:58:02 -0300

    Se limpio el main

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,12 +3,14 @@ package main;
 
 import exceptions.*;
 import modelo.configEmpresa.*;
+import modelo.empresa.Promocion;
+import modelo.empresa.PromocionProducto;
 
 import java.io.IOException;
 import java.util.GregorianCalendar;
 
 public class Main {
 
-    public static void main(String[] args) throws ArchivoNoInciliazadoException, IOException, ClassNotFoundException, DatosLoginIncorrectosException, MesaYaExistenteException, UsuarioNoAutorizadoException, ProductoYaExistenteException, MozoYaAgregadoException {
+    public static void main(String[] args){
     }
 }
