8aab47570a8052b6ecd1ca92a9467579f4f9d592
DavidGentili <DavidGenntili@gmail.com>
2022-11-15 13:06:41 -0300

    se cambio el main

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -9,29 +9,28 @@ import exceptions.mesas.MesaYaOcupadaException;
 import exceptions.mozos.MozoNoActivoException;
 import exceptions.mozos.MozoNoEncontradoException;
 import exceptions.mozos.MozoYaAgregadoException;
-import exceptions.operarios.DatosLoginIncorrectosException;
-import exceptions.operarios.OperarioInactivoException;
-import exceptions.operarios.UsuarioNoAutorizadoException;
+import exceptions.operarios.*;
 import exceptions.persistencia.ArchivoNoInciliazadoException;
 import exceptions.productos.ProductoYaExistenteException;
 import modelo.archivo.Archivo;
 import modelo.configEmpresa.ConfiguracionEmpresa;
+import modelo.configEmpresa.Operario;
 import modelo.gestorEmpresa.*;
 
 import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) throws ArchivoNoInciliazadoException, IOException, ClassNotFoundException, MozoNoEncontradoException, IdIncorrectoException, EmpresaAbiertaException, MesaNoEncontradaException, MozoNoActivoException, MesaYaOcupadaException, ProductoYaExistenteException, UsuarioNoAutorizadoException, OperarioInactivoException, DatosLoginIncorrectosException, MesaYaExistenteException, MozoYaAgregadoException {
+    public static void main(String[] args) throws ArchivoNoInciliazadoException, IOException, ClassNotFoundException, MozoNoEncontradoException, IdIncorrectoException, EmpresaAbiertaException, MesaNoEncontradaException, MozoNoActivoException, MesaYaOcupadaException, ProductoYaExistenteException, UsuarioNoAutorizadoException, OperarioInactivoException, DatosLoginIncorrectosException, MesaYaExistenteException, MozoYaAgregadoException, OperarioNoEncontradoException, ContraseniaIncorrectaException {
         GestorEmpresa empresa = GestorEmpresa.getInstance();
         ConfiguracionEmpresa config = ConfiguracionEmpresa.getInstance();
         Archivo archivo = Archivo.getInstance();
         config.recuperarConfiguracion();
         empresa.recuperarEmpresa();
         archivo.recuperarArchivo();
-
-
-
+        Operario user = config.login("ADMIN", "ADMIN1234");
+        if(config.isPrimerAcceso())
+            config.cambiarContraseniaOperario("ADMIN1234","Admin1234", 0);
 
     }
 
