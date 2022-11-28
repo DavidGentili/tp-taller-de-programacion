0a84692e518ea627b1794d26cf3aea3e010ead6c
DavidGentili <DavidGenntili@gmail.com>
2022-11-28 18:00:35 -0300

    Se documentaron clases, se agregaron asertos e invariantes

--- src/main/java/modelo/gestorEmpresa/Promocion.java
+++ src/main/java/modelo/gestorEmpresa/Promocion.java
@@ -14,31 +14,60 @@ public abstract class Promocion implements Serializable {
 	protected boolean activa;
 
 
+	/**
+	 * Retorna una instancia de promocion
+	 * @param dias dias de la promocion
+	 */
 	public Promocion(String dias) {
 		assert dias != null && !dias.isEmpty() && !dias.isBlank() : "Los dias no pueden ser nulos";
+
 		this.id = nroPromociones;
 		nroPromociones++;
 		this.dias = dias;
 		this.activa = true;
+
+		assert activa == true : "No se asigno correctamente el estado inicial";
+		assert this.dias == dias : "No se asigno correctamente los dias";
 	}
 
+	/**
+	 * Retorna el numero de promociones
+	 * @return numero de promociones
+	 */
 	protected static int getNroPromociones(){
 		return nroPromociones;
 	}
 
+	/**
+	 * Determina el numero de promociones
+	 * @param nroPromociones nuevo numero de promociones
+	 */
 	protected static void setNroPromociones(int nroPromociones){
 		assert nroPromociones >= 0 : "El numero de promociones no puede ser negativo";
 		Promocion.nroPromociones = nroPromociones;
 	}
 
+	/**
+	 * Determina el id de la promocion
+	 * @param id nuevo id de la promocion
+	 */
 	public void setId(int id){
+		assert id >= 0 : "El id no puede ser negativo";
 		this.id = id;
+
+		assert this.id == id : "No se asigno correctamente el id";
 	}
 
+	/**
+	 * Desactiva la promocion
+	 */
 	public void desactivarPromocion() {
 		this.activa = false;
 	 }
 
+	/**
+	 * Activa la promocion
+	 */
 	public void activarPromocion() {
 		this.activa = true;
 	 }
@@ -51,25 +80,67 @@ public abstract class Promocion implements Serializable {
 	public void setDias(String dias) {
 		assert dias != null && !dias.isBlank() && !dias.isEmpty() : "Los dias no pueden ser nulos";
 		this.dias = dias;
-		assert this.dias == dias;
+		assert this.dias == dias : "No se asignaron correctamente los dias";
 	}
 
+	/**
+	 * Retorna el id de la promocion
+	 * @return id de la promocion
+	 */
 	public int getId() {
 		return id;
 	}
+
+	/**
+	 * Retorna los dias de la promocion
+	 * @return dias de la promocion
+	 */
 	public String getDias() {
 		return dias;
 	}
+
+	/**
+	 * Retorna si la promocion esta activa
+	 * @return si la promocion esta activa
+	 */
 	public boolean isActiva() {
 		return activa;
 	}
 
+	/**
+	 * retorna si la promocion es acumulable
+	 * @return
+	 */
 	public abstract boolean isAcumulable();
 
+	/**
+	 * Retorna el descuento
+	 * pre: pedidos != null
+	 * @param pedidos lista de pedidos
+	 * @return descuento correspondiente
+	 */
 	public abstract double getDescuento(ArrayList<Pedido> pedidos);
 
+	/**
+	 * Retorna si se aplica la promocion
+	 * pre: pedidos != null
+	 * 		formaDePago != null
+	 * @param pedidos lista de pedidos
+	 * @param formaDePago forma de pago
+	 * @return si la promocion es aplicable
+	 */
 	public abstract boolean aplicaPromocion(ArrayList<Pedido> pedidos, FormasDePago formaDePago);
 
+	/**
+	 * Retorna si la promocion es aplicable
+	 * pre: pedidos != null
+	 * 		formaDePago != null
+	 * 		fecha != null
+	 * @param pedidos lista de pedidos
+	 * @param formasDePago forma de pago
+	 * @param fecha fecha de la compra
+	 * @return si la promociones es aplicable
+	 */
 	public abstract boolean aplicaPromocion(ArrayList<Pedido> pedidos, FormasDePago formasDePago, GregorianCalendar fecha);
 
 }
--- src/main/java/modelo/gestorEmpresa/PromocionProducto.java
+++ src/main/java/modelo/gestorEmpresa/PromocionProducto.java
@@ -15,6 +15,19 @@ public class PromocionProducto extends Promocion implements Serializable {
 	private int cantMinima;
 	private double precioUnitario;
 
+	/**
+	 * Instancia una promocion de producto
+	 * pre: dias != null && !dias.isEmpty && !dias.blank
+	 * 		producto != null
+	 * 		cantMin >= 0
+	 * 		precion unitario >= 0
+	 * @param dias dias de la promocion
+	 * @param producto producto de la promocion
+	 * @param dosPorUno si la promociones es dos por uno
+	 * @param dtoPorCant si la promocion es descuento por cantidad
+	 * @param cantMin cantidad minima
+	 * @param precioUnitario precio unitario
+	 */
 	public PromocionProducto(String dias, Producto producto, boolean dosPorUno, boolean dtoPorCant, int cantMin, double precioUnitario){
 		super(dias);
 		assert dosPorUno || dtoPorCant : "Ambos no pueden ser falsos";
@@ -28,6 +41,17 @@ public class PromocionProducto extends Promocion implements Serializable {
 		this.precioUnitario = precioUnitario;
 	}
 
+	/**
+	 * Instancia una promocion de producto
+	 * pre: dias != null && !dias.isEmpty && !dias.blank
+	 * 		producto != null
+	 * 		cantMin >= 0
+	 * 		precion unitario >= 0
+	 * @param dias dias de la promocion
+	 * @param producto producto de la promocion
+	 * @param cantMin cantidad minima
+	 * @param precioUnitario precio unitario
+	 */
 	public PromocionProducto(String dias, Producto producto, int cantMin, double precioUnitario) {
 		super(dias);
 		assert producto != null : "El producto no puede ser nulo";
@@ -39,6 +63,13 @@ public class PromocionProducto extends Promocion implements Serializable {
 		this.precioUnitario = precioUnitario;
 	}
 
+	/**
+	 * Instancia una promocion de producto
+	 * pre: dias != null && !dias.isEmpty && !dias.blank
+	 * 		producto != null
+	 * @param dias dias de la promocion
+	 * @param producto producto de la promocion
+	 */
 	public PromocionProducto(String dias, Producto producto){
 		super(dias);
 		assert producto != null : "El producto no puede ser nulo";
@@ -46,61 +77,117 @@ public class PromocionProducto extends Promocion implements Serializable {
 		this.dosPorUno = true;
 	}
 
+	/**
+	 * Determina el producto de la promocion
+	 * @param producto productp de la promocion
+	 */
 	public void setProducto(Producto producto) {
 		this.producto = producto;
 		invariante();
 	}
 
+	/**
+	 * Determina si la promocion es dos por uno
+	 * @param dosPorUno si la promocion es dos por uno
+	 */
 	public void setDosPorUno(boolean dosPorUno) {
 		this.dosPorUno = dosPorUno;
 		invariante();
 	}
 
+	/**
+	 * Determina si la promocion es de descuento por cantidad
+	 * @param dtoPorCant si la promocion es de descuento por cantidad
+	 */
 	public void setDtoPorCant(boolean dtoPorCant) {
 		this.dtoPorCant = dtoPorCant;
 		invariante();
 	}
 
+	/**
+	 * Determina la cantidad minima de productos
+	 * pre : cantMinima >= 0
+	 * @param cantMinima cantidad minima de productos
+	 */
 	public void setCantMinima(int cantMinima) {
+		assert cantMinima >= 0 : "La cantidad minima debe ser mayor o igual a cero";
 		this.cantMinima = cantMinima;
+
 		invariante();
+		assert this.cantMinima == cantMinima : "No se asigno correctamente la cantidad minima";
 	}
 
+	/**
+	 * Determina el precio unitario
+	 * pre : precioUnitario >= 0
+	 * @param precioUnitario Precio unitario
+	 */
 	public void setPrecioUnitario(double precioUnitario) {
+		assert precioUnitario >= 0 : "El precio unitario no puede ser negativo";
 		this.precioUnitario = precioUnitario;
 		invariante();
+		assert this.precioUnitario == precioUnitario : "No se asigno correctamente el precio unitario";
 	}
 
+	/**
+	 * Retorna si la promociones es de dos por uno
+	 * @return si la promociones es de dos por uno
+	 */
 	public boolean isDosPorUno() {
 		return dosPorUno;
 	}
 
+	/**
+	 * Retorna si la promocion es de descuento por cantidad
+	 * @return si la promocion es de descuento por cantidad
+	 */
 	public boolean isDtoPorCant() {
 		return dtoPorCant;
 	}
 
+	/**
+	 * Retorna la cantidad minima de la promocion
+	 * @return cantidad minima de la promocion
+	 */
 	public int getCantMinima() {
 		return cantMinima;
 	}
 
+	/**
+	 * Retorna el precio unitario de la promocion
+	 * @return precio unitario de la promocion
+	 */
 	public double getPrecioUnitario() {
 		return precioUnitario;
 	}
 
+	/**
+	 * Retorna el producto de la promocion
+	 * @return producto de la promocion
+	 */
 	public Producto getProducto(){ return producto;}
 
-	
-	private void invariante(){
-		assert this.dosPorUno | this.dtoPorCant : "Dos por uno y dto por cantidad no pueden ser nulos en simultaneo";
-	}
-
+	/**
+	 * Retorna si la promocion es acumulable
+	 * @return si a promocion es acumulable
+	 */
 	public boolean isAcumulable(){
 		return true;
 	}
 
 
+	/**
+	 * Retorna si la promocion aplica con una lista de pedidos y una forma de pago
+	 * pre: pedidos != null
+	 * 		formaDePago != null
+	 * @param pedidos lista de pedidos
+	 * @param formaDePago forma de pago
+	 * @return si la promocion aplica
+	 */
 	@Override
 	public boolean aplicaPromocion(ArrayList<Pedido> pedidos, FormasDePago formaDePago) {
+		assert pedidos != null : "La lista de pedidos no puede ser nula";
+		assert formaDePago != null : "La forma de pago no puede ser nula";
 		if(!activa || !FacturaHelpers.correspondeDia(dias))
 			return false;
 		boolean aplica = false;
@@ -113,8 +200,21 @@ public class PromocionProducto extends Promocion implements Serializable {
 		return aplica;
 	}
 
+	/**
+	 * Retorna si la promocion es aplicable
+	 * pre: pedidos != null
+	 * 		formaDePago != null
+	 * 		fecha != null
+	 * @param pedidos lista de pedidos
+	 * @param formaDePago forma de pago
+	 * @param fecha fecha de la compra
+	 * @return si la promociones es aplicable
+	 */
 	@Override
-	public boolean aplicaPromocion(ArrayList<Pedido> pedidos, FormasDePago formasDePago, GregorianCalendar fecha){
+	public boolean aplicaPromocion(ArrayList<Pedido> pedidos, FormasDePago formaDePago, GregorianCalendar fecha){
+		assert pedidos != null : "La lista de pedidos no puede ser nula";
+		assert formaDePago != null : "La forma de pago no puede ser nula";
+		assert fecha != null : "La fecha no puede ser nula";
 		if(!activa || !FacturaHelpers.correspondeDia(dias, fecha))
 			return false;
 		boolean aplica = false;
@@ -127,14 +227,28 @@ public class PromocionProducto extends Promocion implements Serializable {
 		return aplica;
 	}
 
+	/**
+	 * Obtiene el descuento correspondiente
+	 * pre : pedidos != null
+	 * @param pedidos lista de pedidos
+	 * @return descuenco correspondiente
+	 */
 	public double getDescuento(ArrayList<Pedido> pedidos){
+		assert pedidos != null : "Los pedidos no pueden ser nulos";
 		if(dosPorUno)
 			return calculaDescuentoDosPorUno(pedidos);
 		else
 			return calculoDescuentoPorCantidad(pedidos);
 	}
 
+	/**
+	 * Calculo los descuentos dos por uno
+	 * pre: pedidos != null
+	 * @param pedidos lista de pedidos
+	 * @return descuento correspondiente
+	 */
 	private double calculaDescuentoDosPorUno(ArrayList<Pedido> pedidos){
+		assert pedidos != null : "Los pedidos no pueden ser nulos";
 		double desc = 0;
 		for (Pedido pedido : pedidos){
 			if(pedido.getProducto().getId() == producto.getId())
@@ -143,7 +257,14 @@ public class PromocionProducto extends Promocion implements Serializable {
 		return desc;
 	}
 
+	/**
+	 * Calcula el descueno de decuentos por cantidad
+	 * pre: pedidos != null
+	 * @param pedidos lista de pedidos
+	 * @return decuento correspondiente
+	 */
 	private double calculoDescuentoPorCantidad(ArrayList<Pedido> pedidos){
+		assert pedidos != null : "Los pedidos no pueden ser nulos";
 		double desc = 0;
 		for (Pedido pedido : pedidos){
 			if(pedido.getProducto().getId() == producto.getId() && pedido.getCantidad() >= cantMinima)
@@ -152,8 +273,24 @@ public class PromocionProducto extends Promocion implements Serializable {
 		return desc;
 	}
 
+	/**
+	 * Retorna la informacion de la promocion en forma de string
+	 * @return informacion de la promocion en forma de string
+	 */
 	@Override
 	public String toString() {
 		return String.format("| %-12s %-5s  %s |", producto.getNombre(), dosPorUno ? "2x1" : "xCant", dias);
+	}
+
+	/**
+	 * invariante de clase
+	 */
+	private void invariante(){
+		assert this.dosPorUno | this.dtoPorCant : "Dos por uno y dto por cantidad no pueden ser nulos en simultaneo";
+		if(dtoPorCant){
+			assert cantMinima >= 0 : "La cantidad minima no puede ser negativa";
+			assert precioUnitario >= 0 : "El precio unitario no puede ser negativo";
+		}
+
 	}
 }
